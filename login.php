db7a1f7d8c60e33b5e43c7a3eadcedeeee166d12
nackzess <nackzess2539@gmail.com>
2021-08-02 10:53:49 +0700

    lohin title

--- login.php
+++ login.php
@@ -22,7 +22,7 @@ if (isset($_SESSION['student_login'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Login - SB Admin</title>
+    <title>Login - Coco Admin</title>
     <link href="css/styles.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
 </head>
